da6a7b85ea758f0a139a107fad003d6dc2745bbe
Pamela <pamelaeugeniamartin@gmail.com>
2022-07-29 10:39:52 -0300

    Con saludo personalizado

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,18 @@
 const logoutElement = document.getElementById('test')
+const saludo = document.getElementById('welcomeGreeting')
+
+
+let localStore = localStorage.getItem('users');
+let usersList = [];
+
+if (localStore) {
+    usersList = JSON.parse(localStore);
+}
+usersList.forEach((user) => {
+    saludo.innerHTML = `@Welcome ${user.name}`
+    console.log(`Hola ${user.name}`)
+});
+
 
 const logoutUser = () => {
     localStorage.removeItem('user')
